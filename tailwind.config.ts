280c373d824dcc74c45d0d147f38e6c926ad8ef8
Kusal K C <kusal.kc@student.torrens.edu.au>
2023-12-03 17:30:17 +1000

    feat: add daisyui and react-icons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,5 +10,22 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#6d28d9",
+          secondary: "#1f2937",
+          accent: "#4c1d95",
+          neutral: "#d1d5db",
+          "base-100": "#ffffff",
+          info: "#1d4ed8",
+          success: "#166534",
+          warning: "#fcd34d",
+          error: "#e11d48",
+        },
+      },
+    ],
+  },
 } satisfies Config;
